0583727db1810e050525573d21a4afc261a5daf3
PauliLuna <luna.paulina12@gmail.com>
2023-11-22 22:59:36 -0300

    Ignorar feaures coverage y eliminar pages

--- features/steps/ahorcado_steps.py
+++ features/steps/ahorcado_steps.py
@@ -1,4 +1,4 @@
-from behave import *
+from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
