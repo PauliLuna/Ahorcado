669519cae1e28895ca958e090fd332bea89911e0
PauliLuna <luna.paulina12@gmail.com>
2023-11-23 13:34:58 -0300

    Fix scenario ingresar letra incorrecta

--- features/ahorcado.feature
+++ features/ahorcado.feature
@@ -1,50 +1,50 @@
 Feature: Juego del Ahorcado
 
-  # Scenario Outline: Elegir opcion elegir por tematica o por nivel
-  #   Given Que estoy en la página del Juego del Ahorcado
-  #   When Eligo "<opcion>"
-  #   And Hago clic en el boton Continuar
-  #   Then Deberia ser redirigido a la proxima pagina
-  #   And Deberia ver el label "<eleccion>"
+   Scenario Outline: Elegir opcion elegir por tematica o por nivel
+     Given Que estoy en la página del Juego del Ahorcado
+     When Eligo "<opcion>"
+     And Hago clic en el boton Continuar
+     Then Deberia ser redirigido a la proxima pagina
+     And Deberia ver el label "<eleccion>"
 
-  #   Examples: Opciones
-  #     | opcion                         | eleccion         |
-  #     | Jugar por temáticas            | Elegir temática: |
-  #     | Jugar por nivel de dificultad  | Elegir nivel:    |
+     Examples: Opciones
+       | opcion                         | eleccion         |
+       | Jugar por temáticas            | Elegir temática: |
+       | Jugar por nivel de dificultad  | Elegir nivel:    |
 
-  # Scenario Outline: Elegir diferentes tematicas o niveles
-  #   Given Que estoy en la página del Juego del Ahorcado
-  #   When Eligo "<opcion>"
-  #   And Hago clic en el boton Continuar
-  #   Then Deberia ser redirigido a la proxima pagina
-  #   And Deberia ver la opcion "<seleccion>"
+   Scenario Outline: Elegir diferentes tematicas o niveles
+     Given Que estoy en la página del Juego del Ahorcado
+     When Eligo "<opcion>"
+     And Hago clic en el boton Continuar
+     Then Deberia ser redirigido a la proxima pagina
+     And Deberia ver la opcion "<seleccion>"
 
-  #   Examples: Tematicas
-  #     | opcion                         | seleccion   |
-  #     | Jugar por temáticas            | Animales    |
-  #     | Jugar por temáticas            | Comida      |
-  #     | Jugar por temáticas            | Paises      |
-  #     | Jugar por temáticas            | Profesiones |
-  #     | Jugar por temáticas            | Deportes    |
-  #     | Jugar por nivel de dificultad  | Fácil       |
-  #     | Jugar por nivel de dificultad  | Medio       |
-  #     | Jugar por nivel de dificultad  | Difícil     |
+     Examples: Tematicas
+       | opcion                         | seleccion   |
+       | Jugar por temáticas            | Animales    |
+       | Jugar por temáticas            | Comida      |
+       | Jugar por temáticas            | Paises      |
+       | Jugar por temáticas            | Profesiones |
+       | Jugar por temáticas            | Deportes    |
+       | Jugar por nivel de dificultad  | Fácil       |
+       | Jugar por nivel de dificultad  | Medio       |
+       | Jugar por nivel de dificultad  | Difícil     |
 
   Scenario: Ingresar letra incorrecta
     Given Que estoy en la página del Juego del Ahorcado
     When Eligo "Jugar por temáticas"
-    And Hago clic en el botón Continuar
-    Then Debería ser redirigido a la próxima página
-    And Debería ver el label "Elegir temática:"
+    And Hago clic en el boton Continuar
+    Then Deberia ser redirigido a la proxima pagina
+    And Deberia ver el label "Elegir temática:"
 
-    Given Que estoy en la página Elegir opcion
-    When Selecciono la temática "Animales"
-    And Hago clic en el botón Comenzar Juego
-    Then Debería ser redirigido a la próxima página de juego
-    And Debería ver el label "Ingresar letra o palabra:"
+    Given Que estoy en la pagina Elegir opcion
+    When Selecciono la tematica "Animales"
+    And Hago clic en el boton Comenzar Juego
+    Then Deberia ser redirigido a la proxima pagina de juego
+    And Deberia ver el label "Ingresar letra o palabra:"
 
-    Given Que estoy en la página Jugar
+    Given Que estoy en la pagina Jugar
     When Ingreso la letra "X"
-    And Hago clic en el botón Adivinar
-    Then Debería ver el mensaje de letra incorrecta
-    And Debería ver que las vidas disminuyen en 1
+    And Hago clic en el boton Adivinar
+    Then Deberia ver el mensaje de letra incorrecta
+    And Deberia ver que las vidas disminuyen en 1
--- features/steps/ahorcado_steps.py
+++ features/steps/ahorcado_steps.py
@@ -9,7 +9,7 @@ ELEGIR_URL = "https://metodologias-agiles-juego-ahorcado.onrender.com/elegir_opc
 JUGAR_URL = "https://metodologias-agiles-juego-ahorcado.onrender.com/jugar"
 
 def esperar_elemento(context, by, value):
-    return WebDriverWait(context.driver, 50).until(
+    return WebDriverWait(context.driver, 30).until(
         EC.presence_of_element_located((by, value))
     )
 
@@ -17,6 +17,13 @@ def obtener_opciones_dropdown(context, dropdown_id):
     dropdown = esperar_elemento(context, By.ID, dropdown_id)
     select = Select(dropdown)
     return [option.text for option in select.options]
+
+def obtener_numero_vidas(context):
+    vidas_elemento = WebDriverWait(context.driver, 30).until(
+    EC.presence_of_element_located((By.XPATH, "//p[contains(text(), 'Vidas restantes')]"))
+)
+    vidas_texto = vidas_elemento.text
+    return int(''.join(filter(str.isdigit, vidas_texto)))
 
 # features/steps/ahorcado_steps.py
 @given('Que estoy en la página del Juego del Ahorcado')
@@ -51,73 +58,47 @@ def step_then(context, seleccion):
     assert seleccion in opciones
 #-----
 
-@when('Eligo {string}')
-def step_when(context, opcion):
-    dropdown = esperar_elemento(context, By.ID, "Jugar por temáticas")
-    select = Select(dropdown)
-    select.select_by_visible_text(opcion)
-
-@when('Hago clic en el botón Continuar')
-def step_impl(context) :
-    boton = esperar_elemento(context, By.XPATH, "//button[contains(text(), 'Continuar')]")
-    boton.click()
-
-@then('Debería ser redirigido a la próxima página')
-def step_then(context):
+@given('Que estoy en la pagina Elegir opcion')
+def step_given(context):
     assert ELEGIR_URL in context.driver.current_url
 
-@then('Debería ver el label {string}')
-def step_then(context, string):
-    esperar_elemento(context, By.XPATH, f"//*[contains(text(), '{string}')]")
-    label = context.driver.find_element(By.XPATH, f"//*[contains(text(), '{string}')]")
-    assert label.is_displayed()
-
-
-@given('Que estoy en la página Elegir opcion')
-def step_given(context):
-    context.driver = webdriver.Chrome()
-    context.driver.get(ELEGIR_URL)
-
-@when('Selecciono la temática {string}')
+@when('Selecciono la tematica "{string}"')
 def step_seleccionar_tematica(context, string):
-    dropdown_tematica = esperar_elemento(context, By.ID, "Animales")
+    dropdown_tematica = esperar_elemento(context, By.ID, "seleccion")
     select_tematica = Select(dropdown_tematica)
     select_tematica.select_by_visible_text(string)
 
-@when('Hago clic en el botón Comenzar Juego')
+@when('Hago clic en el boton Comenzar Juego')
 def step_comenzar_juego(context):
     boton_comenzar = esperar_elemento(context, By.XPATH, "//button[contains(text(), 'Comenzar Juego')]")
     boton_comenzar.click()
 
-@then('Debería ser redirigido a la próxima página de juego')
+@then('Deberia ser redirigido a la proxima pagina de juego')
 def step_then(context):
     assert JUGAR_URL in context.driver.current_url
 
 
-@given('Que estoy en la página Jugar')
+@given('Que estoy en la pagina Jugar')
 def step_given(context):
-    context.driver = webdriver.Chrome()
-    context.driver.get(JUGAR_URL)
+    assert JUGAR_URL in context.driver.current_url
 
-@when('Ingreso la letra {string}')
+@when('Ingreso la letra "{letra}"')
 def step_ingresar_letra(context, letra):
-    input_letra = esperar_elemento(context, By.ID, "entrada-letra")
+    input_letra = esperar_elemento(context, By.NAME, "entrada")
     input_letra.send_keys(letra)
 
-@when('Hago clic en el botón Adivinar')
+@when('Hago clic en el boton Adivinar')
 def step_adivinar(context):
     boton_adivinar = esperar_elemento(context, By.XPATH, "//button[contains(text(), 'Adivinar')]")
     boton_adivinar.click()
 
-@then('Debería ver el mensaje de letra incorrecta')
+@then('Deberia ver el mensaje de letra incorrecta')
 def step_ver_mensaje_letra_incorrecta(context):
     mensaje_letra_incorrecta = esperar_elemento(context, By.CLASS_NAME, "mensaje-advertencia")
-    assert "La letra X es incorrecta." in mensaje_letra_incorrecta.text  # Ajusta según tu mensaje específico
+    assert "La letra x es incorrecta. Perdiste 1 vida." in mensaje_letra_incorrecta.text  # Ajusta según tu mensaje específico
 
-@then('Debería ver que las vidas disminuyen en 1')
+@then('Deberia ver que las vidas disminuyen en 1')
 def step_ver_vidas_disminuyen(context):
-    vidas_restantes = esperar_elemento(context, By.ID, "vidas-restantes")
-    assert int(vidas_restantes.text) == context.vidas_restantes - 1  # Ajusta según la lógica de tu aplicación
-
-
-
+    vidas_restantes =  obtener_numero_vidas(context)
+    print(vidas_restantes)
+    assert vidas_restantes == 6
